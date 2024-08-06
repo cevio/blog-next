34da31f50ff929fb89ba002f7304e731b1def754
沈赟杰 <shenyunjie@znrank.com>
2024-08-06 14:06:20 +0800

    update

--- src/middlewares/user.info.ts
+++ src/middlewares/user.info.ts
@@ -1,7 +1,7 @@
-import CacheServer from "@braken/cache";
 import { Middleware } from "@braken/http";
 import { Next, Context } from "koa";
 import { UserCache, UserContextState } from "../caches/user.cache";
+import { LoginService } from "../services/login.service";
 
 declare module 'koa' {
   interface BaseContext {
@@ -11,20 +11,22 @@ declare module 'koa' {
 
 @Middleware.Injectable
 export class AuthorizeWare extends Middleware {
-  @Middleware.Inject(CacheServer)
-  private readonly cache: CacheServer;
-
   @Middleware.Inject(UserCache)
   private readonly user: UserCache;
+
+  @Middleware.Inject(LoginService)
+  private readonly login: LoginService;
 
   public async use(ctx: Context, next: Next) {
     let token = ctx.cookies.get('authorization', { signed: true });
     if (!token) token = ctx.get('authorization');
     if (!token) return await next();
 
-    const userTokenCacheKey = '/login/token/' + token;
-    const id: number = await this.cache.read(userTokenCacheKey);
+    const { has, read } = this.login.useToken(token);
 
+    if (!(await has())) return await next();
+
+    const id = await read();
     if (id) {
       const user = await this.user.$read({ id: id.toString() });
       ctx.user = user;
--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,7 +1,7 @@
+import CacheServer from '@braken/cache';
 import { Component } from '@braken/injection';
 import { UserVariable } from '../variables/user.var';
 import { UserContextState } from '../caches/user.cache';
-import CacheServer from '@braken/cache';
 
 @Component.Injectable
 export class LoginService extends Component {
@@ -11,42 +11,85 @@ export class LoginService extends Component {
   @Component.Inject(CacheServer)
   private readonly cache: CacheServer;
 
+  static createTokenKey(token: string) {
+    return '/login/token/' + token;
+  }
+
+  static createAccountKey(id: number) {
+    return '/login/account/' + id;
+  }
+
+  public useToken(token: string) {
+    const key = LoginService.createTokenKey(token);
+    return {
+      key,
+      has: () => this.cache.has(key),
+      read: (): Promise<number> => this.cache.read(key),
+      remove: () => this.cache.remove(key),
+      write: (id: number, expire?: number) => this.cache.write(key, id, expire),
+    }
+  }
+
+  public useId(id: number) {
+    const key = LoginService.createAccountKey(id);
+    return {
+      key,
+      has: () => this.cache.has(key),
+      read: (): Promise<string> => this.cache.read(key),
+      remove: () => this.cache.remove(key),
+      write: (token: string, expire?: number) => this.cache.write(key, token, expire),
+    }
+  }
+
+  private getExpire() {
+    return Date.now() + this.variable.get('loginExpire') * 24 * 60 * 60 * 1000;
+  }
+
   public async updateCache(user: UserContextState) {
-    const maxAgeSec = Date.now() + this.variable.get('loginExpire') * 24 * 60 * 60 * 1000;
-    const userTokenCacheKey = '/login/token/' + user.token;
-    const userAccountCacheKey = '/login/account/' + user.id;
+    const IdMachine = this.useId(user.id);
+    const TokenMachine = this.useToken(user.token);
+    const maxAgeSec = this.getExpire();
 
     // 清理旧缓存
-    const token = await this.cache.read(userAccountCacheKey);
-    if (token) {
-      await this.cache.remove('/login/token/' + token);
+    if (await IdMachine.has()) {
+      const token = await IdMachine.read();
+      if (token) {
+        const { remove, has } = this.useToken(token);
+        if (await has()) {
+          await remove();
+        }
+      }
     }
 
     // 写入新缓存
-    await this.cache.write(userTokenCacheKey, user.id, maxAgeSec);
-    await this.cache.write(userAccountCacheKey, user.token, maxAgeSec);
+    await TokenMachine.write(user.id, maxAgeSec);
+    await IdMachine.write(user.token, maxAgeSec);
 
     return maxAgeSec;
   }
 
   public async removeCache(id: number) {
-    const userAccountCacheKey = '/login/account/' + id;
-    const token = await this.cache.read(userAccountCacheKey);
-    if (token) {
-      const userTokenCacheKey = '/login/token/' + token;
-      await this.cache.remove(userTokenCacheKey);
-      await this.cache.remove(userAccountCacheKey);
+    const IdMachine = this.useId(id);
+    if (await IdMachine.has()) {
+      const token = await IdMachine.read();
+      if (token) {
+        const TokenMachine = this.useToken(token);
+        if (await TokenMachine.has()) {
+          await TokenMachine.remove();
+        }
+      }
+      await IdMachine.remove();
     }
   }
 
   public async createCache(user: UserContextState) {
-    const maxAgeSec = Date.now() + this.variable.get('loginExpire') * 24 * 60 * 60 * 1000;
-    const userTokenCacheKey = '/login/token/' + user.token;
-    const userAccountCacheKey = '/login/account/' + user.id;
+    const IdMachine = this.useId(user.id);
+    const TokenMachine = this.useToken(user.token);
+    const maxAgeSec = this.getExpire();
 
     // 写入新缓存
-    await this.cache.write(userTokenCacheKey, user.id, maxAgeSec);
-    await this.cache.write(userAccountCacheKey, user.token, maxAgeSec);
+    await IdMachine.write(user.token, maxAgeSec);
+    await TokenMachine.write(user.id, maxAgeSec);
 
     return maxAgeSec;
   }
